953149b67ebd43de07412165f29b804923b2dc93
Kamalesh Pathy VA <53052534+kamalesh-pathy-va@users.noreply.github.com>
2020-06-30 13:02:15 +0530

    Removed unwanted import statements

--- mainClassifier.py
+++ mainClassifier.py
@@ -5,7 +5,6 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 import pickle
-import random
 
 with open("trainingData.json", "r") as file:
     data = json.load(file)
